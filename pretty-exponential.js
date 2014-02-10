a6b92f7e01d4a74571c0fe9476d780bf2a00ca76
bmcustodio <bruno@brunomcustodio.com>
2014-02-10 21:38:13 +0000

    Updating filenames.

--- pretty-exponential.js
+++ pretty-exponential.js
@@ -1,6 +1,55 @@
 /*!
- *  pretty-exponential 1.0.0 (https://github.com/bmcustodio/pretty-exponential)
+ *  pretty-exponential 1.0.1 (https://github.com/bmcustodio/pretty-exponential)
  *  Copyright 2014 Bruno M. Custódio
  *  MIT License
  */
-(function(){var c={"0":"⁰","1":"¹","2":"²","3":"³","4":"⁴","5":"⁵","6":"⁶","7":"⁷","8":"⁸","9":"⁹","-":"⁻"};var b=/(\-?\d(?:\.\d+)?)e\+?(\-?\d+)/;var a=function(h,f){var j=Number(h).toExponential(f).match(b);if(!j){throw new TypeError("Non-numerical or non-finite value found")}var e=j[1],d=j[2];if(parseInt(d,10)===0){return e}var i="";for(var g=0;g<d.length;g+=1){i+=c[d[g]]}return e+"×10"+i};if(typeof module!=="undefined"){module.exports=a}else{window.prettyExponential=a}}());
+
+(function () {
+
+    'use strict';
+
+    var exponents = {
+        '0': '⁰',
+        '1': '¹',
+        '2': '²',
+        '3': '³',
+        '4': '⁴',
+        '5': '⁵',
+        '6': '⁶',
+        '7': '⁷',
+        '8': '⁸',
+        '9': '⁹',
+        '-': '⁻'
+    };
+
+    var regex = /(\-?\d(?:\.\d+)?)e\+?(\-?\d+)/;
+
+    var prettyExponential = function (number, precision) {
+        var matches = Number(number).toExponential(precision).match(regex);
+
+        if (!matches) {
+            throw new TypeError('Non-numerical or non-finite value found');
+        }
+
+        var significand = matches[1], oldExponent = matches[2];
+
+        if (parseInt(oldExponent, 10) === 0) {
+            return significand;
+        }
+
+        var exponent = '';
+
+        for (var index = 0; index < oldExponent.length; index += 1) {
+            exponent += exponents[oldExponent[index]];
+        }
+
+        return significand + '×' + '10' + exponent;
+    };
+
+    if (typeof module !== 'undefined') {
+        module.exports = prettyExponential;
+    } else {
+        window.prettyExponential = prettyExponential;
+    }
+
+}());
